43278ff4329b228da698791dd2fcffbdebf9a71e
MaxCojocari <92053176+MaxCojocari@users.noreply.github.com>
2022-01-18 12:56:22 +0200

    Update ML_express_hw1.sql

--- ML_express_hw1.sql
+++ ML_express_hw1.sql
@@ -1,9 +1,9 @@
 SELECT authors.name AS author_name,
-			papers.year AS publish_year,
-			papers.title AS paper_title,
-			papers.pdf_name AS pdf_name,
-			papers.abstract AS paper_abstract,
-			papers.paper_text AS paper_text
+       papers.year AS publish_year,
+       papers.title AS paper_title,
+       papers.pdf_name AS pdf_name,
+       papers.abstract AS paper_abstract,
+       papers.paper_text AS paper_text
 FROM authors
 LEFT JOIN papers ON papers.id = authors.id;
 			
